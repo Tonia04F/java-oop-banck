ca6b08088a542d3fb2d997e9220209f5375ad4a4
tonia <tonia@cia.como>
2022-08-09 18:20:28 +0200

    inizio

---
+++ src/banck/Banck.java
@@ -0,0 +1,10 @@
+package banck;
+
+public class Banck {
+
+	/*Crea nuovo conto assegnando all utente un umero di conto random
+	 * chiedigli se vuole prelevare o versare
+	 * restituisci il saldo aggiornato*/
+	
+	
+}
---
+++ src/banck/Conto.java
@@ -0,0 +1,11 @@
+package banck;
+
+public class Conto {
+	/*quando creo un nuovo conto il saldo deve essere 0
+	 * il numero di conto puo essere visto ma non modificato
+	 * il nome del proprietario puo essere sia vsto che scritto
+	 * il saldo posso leggerlo ma non modificarlo, creare quindi metodi che permettano di prelevare e versare denaro*/
+	
+	private String 
+
+}
